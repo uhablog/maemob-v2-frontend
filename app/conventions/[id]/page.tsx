0bb0ff33c90f2d9c6e6c84291f5aba45e71bb026
uhablog <uha.blog98@gmail.com>
2025-03-15 13:50:38 +0900

    【大会詳細画面】 大会名の取得 #17

--- app/conventions/[id]/page.tsx
+++ app/conventions/[id]/page.tsx
@@ -2,17 +2,26 @@
 
 import GamesList from '@/components/conventions/GamesList';
 import ConventionSummary from '@/components/conventions/Summary';
+import useFetchData from '@/hooks/useFetchData';
+import { Convention } from '@/types/convention';
 import { useParams } from 'next/navigation';
 import { useState } from 'react';
+
+const API_ENDPOINT = process.env.NEXT_PUBLIC_API_ENDPOINT ?? 'http://localhost:3000';
 
 export default function Page() {
   const params = useParams<{ id: string }>();
   const [activeTab, setActiveTab] = useState("概要");
 
+  const { data: convention, loading, error } = useFetchData<Convention>(`${API_ENDPOINT}/api/convention/${params.id}`);
+
+  if (loading) return <div className='text-center p-4'>Loading...</div>
+  if (error) return <div className='text-red-500 text-center p-4'>{error}</div>
+
   return (
     <div className="max-w-4xl mx-auto p-6">
       <div className="border p-4 rounded-lg shadow-sm flex-grow">
-        <h2 className="font-semibold text-lg">Unknown</h2>
+        <h2 className="font-semibold text-lg">{convention?.name}</h2>
         <div className='flex space-x-4 mt-2 border-b'>
           {["概要", "試合"].map((tab) => (
             <button

2d8a3965413ff8a5de87d98f7e08a807ffa5209c
uhablog <uha.blog98@gmail.com>
2025-03-16 11:06:16 +0900

    試合情報の取得

--- app/conventions/[id]/matches/[match_id]/page.tsx
+++ app/conventions/[id]/matches/[match_id]/page.tsx
@@ -1,10 +1,30 @@
 'use client';
 
+import useFetchData from "@/hooks/useFetchData";
+import { Match } from "@/types/match";
+import { useParams } from "next/navigation";
+
+const API_ENDPOINT = process.env.NEXT_PUBLIC_API_ENDPOINT ?? 'http://localhost:3000';
+
 export default function Page() {
+
+  const params = useParams<{
+    id: string
+    match_id: string
+  }>();
+
+  const { data: match, loading, error } = useFetchData<Match>(`${API_ENDPOINT}/api/conventions/${params.id}/match/${params.match_id}`)
+
+  if (loading) return <div className='text-center p-4'>Loading...</div>
+  if (error) return <div className='text-red-500 text-center p-4'>{error}</div>
 
   return (
     <>
-      match page
+      <div className="border p-4 rounded-lg shadow-sm flex-grow">
+        <h2 className="font-semibold text-lg">
+          {match?.homePlayerId ?? 'Unknown'} {match?.homeScore} - {match?.awayScore} {match?.awayPlayerId ?? 'Unknown'}
+        </h2>
+      </div>
     </>
   )
 };
